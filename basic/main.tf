5fe76c0df210cf79371a5ddc9fb7cf6c54d6b4aa
luis486 <luis.murcia1@u.icesi.edu.co>
2024-03-14 13:10:19 -0500

    SSH
    Add network security group and  network interface security group to connect by ssh

--- basic/main.tf
+++ basic/main.tf
@@ -54,6 +54,30 @@ resource "azurerm_network_interface" "app_interface" {
   ]
 }
 
+resource "azurerm_network_security_group" "nsg" {
+  name                = "myNetworkSecurityGroup"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+# Connect the security group to the network interface
+resource "azurerm_network_interface_security_group_association" "isga" {
+  network_interface_id      = azurerm_network_interface.app_interface.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
+}
+
 resource "azurerm_linux_virtual_machine" "linux_vm" {
   name                = "linuxvm"
   resource_group_name = azurerm_resource_group.app_grp.name
